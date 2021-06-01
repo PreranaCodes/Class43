e9ae463b864d7ceab30c97eb4deb1d528407f0b1
AasthaSharma-git <67312804+AasthaSharma-git@users.noreply.github.com>
2021-06-01 11:06:35 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,108 +1,6 @@
 class Game{
    
-    constructor(){}class Game{
-
-    constructor(){
-
-    }
-
-
-    getState(){
-      
-        var gameStateRef=database.ref('gameState');
-
-        gameStateRef.on("value",function(data){
-              
-              gameState=data.val();
-        });
-
-    }
-
-   start(){
-    if(gameState===0){
-        form=new Form();
-        form.display();
-
-        player = new Player();
-
-        player.getCount();
-    }
-    car1= createSprite(375,200,100,100);
-    car1.addImage(carImg1);
-    car2= createSprite(575,200,100,100);
-    car2.addImage(carImg2);
-    car3= createSprite(775,200,100,100);
-    car3.addImage(carImg3);
-    car4= createSprite(975,200,100,100);
-    car4.addImage(carImg4);
-    
-    cars=[car1,car2,car3,car4];
-
-}
-
-play(){
-    background('white');
-    form.disappear();
-    textSize(50);
-    text('Game Start!',100,200);
-
-    Player.getGameInfo();
-
-    var pos=150;
-
-    var y;
-    background('white');
-    if(gameInfo!=undefined){
-
-        for(var i=1;i<=4;i++){
-           
-            var playerIndex="player"+i;
-
-            y = displayHeight-gameInfo[playerIndex].distance;
-
-            cars[i-1].y=y;
-            
-            if(player.index===i){
-                cars[i-1].shapeColor="red";
-                camera.position.x=displayWidth/2  ;
-                camera.position.y=cars[i-1].y;
-             }
-            else{
-                cars[i-1].shapeColor="black";
-            }
-
-            
-
-        }
-
-
-       
-    }
-    
-    if(keyDown(UP_ARROW)){
-        player.distance = player.distance+10;
-        player.updateDetails();
-    }
-
-
-   
-
-   drawSprites();
-
-
-}
-
-
-updateState(state){
-      
-    database.ref('/').update({
-        gameState:state
-    })
-
-}
-      
-
-}
+    constructor(){}
 
 
        getState(){
@@ -116,55 +14,52 @@ updateState(state){
 
     }
 
-   start(){
+   async start(){
 
         if(gameState===0){
 
             player=new Player();
 
+            var playerCountRef=await database.ref('playerCount').once("value");
+
             player.getCount();
             player.getFinishedPlayers();
+
        
             form=new Form();
             form.display();
 
 
-            car1 = createSprite(375,200);
-            car1.addImage("car1",carImg1);
+            car1=createSprite(375,200);
+            car1.addImage(car1Img);
             car1.depth=2;
-            
- 
-            car2 = createSprite(575,200);
-            car2.addImage("car2",carImg2);
-           
- 
-            car3 = createSprite(775,200);
-            car3.addImage("car1",carImg3);
-           
- 
-            car4 = createSprite(975,200);
-            car4.addImage("car4",carImg4);
-            
-            
-            cars = [car1, car2, car3, car4];
+            car2=createSprite(575,200);
+            car2.addImage(car2Img);
+            car3=createSprite(775,200);
+            car3.addImage(car3Img);
+            car4=createSprite(995,200);
+            car4.addImage(car4Img);
 
-            obstacles=createGroup();
 
-           var xPos=0;
-           var yPos=0;
+            cars=[car1,car2,car3,car4];
 
-           for(var i=0;i<=5;i++){
-                xPos=random(200,950);
-                yPos=random(-height*4,height-300);
-                obs=createSprite(xPos,yPos);
-                obs.addImage("obstacle",obsImg);
-                
-                obs.depth=1;
-                obstacles.add(obs);
-                
-                
-           }
-            
+            var obsX=0;
+            var obsY=0;
+
+            obstacleGroup=createGroup();
+
+            for(var i=1;i<=5;i++){
+
+               obsX=random(200,950);
+               obsY=random(-height*4,height-300);
+
+               obstacle=createSprite(obsX,obsY);
+               obstacle.depth=1;
+               obstacle.addImage(obsImg);
+               obstacleGroup.add(obstacle);
+
+               
+            }         
 
          }
         
@@ -176,108 +71,113 @@ updateState(state){
       text('Game Start!',120,100);
       form.disappear();
 
-      Player.getPlayerInfo();//called it with player object
+      Player.getPlayerInfo();
       
       background("black");
       
       if(allPlayers!==undefined){
+
          image(trackImg,0,-displayHeight*4,displayWidth,displayHeight*5);
          drawSprites();
+
          for(var i=1;i<=4;i++){
 
             var playerIndex="player"+i;
 
-            if(player.index===i){
-                
-                fill('red');
-                camera.position.x=displayWidth/2;
-                camera.position.y=cars[i-1].y;
-             }
-             else{
-                 fill('black');
-             }
-
             textAlign(CENTER);
-            text(allPlayers[playerIndex].name,cars[i-1].x,cars[i-1].y+75);
-
             x=allPlayers[playerIndex].xPos;
             y=displayHeight-allPlayers[playerIndex].distance;
-
-           cars[i-1].x=x;
-           cars[i-1].y=y;
-          
-          
+            cars[i-1].x=x;
            
+          
+           if(player.index===i){
+               
+               fill('red');
+               cars[i-1].y=y;
+               camera.position.x=displayWidth/2;
+               camera.position.y=cars[i-1].y;
+              
+            }
+            else{
+               
+               fill('white');
+               var diff=displayHeight-allPlayers[playerIndex].screenSize;
+               if(diff>0){
+                  cars[i-1].y=y-diff;
+               }
+               else{
+                  cars[i-1].y=y+diff;
+               }
+
+               
+              
+            }
+            text(allPlayers[playerIndex].name,cars[i-1].x,cars[i-1].y+75);
        }
       }
-
-      if(player.distance>3516&&passedFinish===false){
-
-
-         player.rank=finishedPlayers+1;
-      
-         Player.updateFinishedPlayers();
-         player.update();
-         
-         passedFinish=true;
-       
-
-
-    
-      }
-      if(passedFinish===false){
-      
+     if(passedFinished===false){
       if(keyIsDown(UP_ARROW)){
-                
-         if(cars[player.index-1].isTouching(obstacles)){
-             slidingSound.play();
-             player.distance=player.distance+2;
-                
-         } 
-         else{
-            player.distance=player.distance+10;
-          
+         if(cars[player.index-1].isTouching(obstacleGroup)){
+            player.distance=player.distance+2;
+            player.update();
+            slidingSound.play();
+         }
+         else {
+         player.distance=player.distance+10;
+         player.update();
+         //player.distance+=10;
+         }
         }
-        player.update();
-        
-   
- }
- 
+
      if(keyIsDown(LEFT_ARROW)){
-
-        if(cars[player.index-1].isTouching(obstacles)){
-            slidingSound.play();
-            player.xPos-=2;
-           
-        } 
+        if(cars[player.index-1].isTouching(obstacleGroup)){
+         player.xPos-=2;
+         player.update();
+         slidingSound.play();
+        }
         else{
-            player.xPos-=5;
-        }  
-        player.update();
-    
-    }
+         player.xPos-=10;
+         player.update();
+        }
+        
+        
+     }
+        
+        
     if(keyIsDown(RIGHT_ARROW)){
-
-        if(cars[player.index-1].isTouching(obstacles)){
-            slidingSound.play();
-            player.xPos+=2;
-        } 
+      if(cars[player.index-1].isTouching(obstacleGroup)){
+         player.xPos+=2;
+         player.update();
+         slidingSound.play();
+        }
         else{
-            player.xPos+=5;
-        } 
-        player.update(); 
-    }
+         player.xPos+=10;
+         player.update();
+        }
+        }
+     } 
+     
+        
+        
+    if(player.distance>displayHeight*5-80&&passedFinished===false){
+           player.rank=finishedPlayers+1;
+           Player.updateFinishedPlayers();
+           passedFinished=true;
+           player.update();
+
+        }
+    
+
+
+
 
    }
- }
-
-  displayRanks(){
+    displayRanks(){
 
       background('lightblue');
-    
+
       camera.position.x=0;
       camera.position.y=0;
-      
 
       imageMode(CENTER);
 
@@ -285,34 +185,37 @@ updateState(state){
       image(silverImg,displayWidth/4,-100+displayHeight/10,225,270);
       image(goldImg,0,-100,250,300);
 
-      textAlign(CENTER);
-      textSize(50);
-      fill('black');
-      
-      for(var i=1;i<=4;i++){
-
+      for(var i=1; i<5;i++){
+         
          var playerIndex="player"+i;
+
          var place=allPlayers[playerIndex].rank;
-         var name=allPlayers[playerIndex].name
-        
+         var name=allPlayers[playerIndex].name;
+
+         textAlign(CENTER);
+         textSize(50);
+
+
          if(place===1){
-             text("1st: "+name,0,85);
+            text("1st: "+name,0,85);
          }
-        else if(allPlayers[playerIndex].rank===2){
-             text("2nd: "+name,displayWidth/4,displayHeight/9+73);
+         else if(place===2){
+            text("2nd: "+name,displayWidth/4,displayHeight/9+73);
          }
-        else if(allPlayers[playerIndex].rank===3){
-             text("3rd: "+name,-displayWidth/4,displayHeight/10+76);
+         else if(place===3){
+            text("3rd: "+name,-displayWidth/4,displayHeight/10+76);
          }
+
          else{
             textSize(30);
-            text("Honorable Mention: "+name,0,225)
-
+            text("Honorable Mention: "+name,0,225);
          }
+        
 
-       }
+         
+      }
 
-   }
+    }
 
    update(state){
       database.ref('/').update({
